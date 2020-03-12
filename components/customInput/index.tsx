7463554106526ae29d01327c940722c253c8d5f0
DNature <divinehycenth@outlook.com>
2020-03-12 16:05:55 +0100

    changed to use netlify functions

--- components/customInput/index.tsx
+++ components/customInput/index.tsx
@@ -1,28 +1,43 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import * as React from "react";
+import PropTypes from "prop-types";
 
 interface Props {
-    placeholder?: string;
-    type: string;
-    id: string;
-    name: string;
+  placeholder?: string;
+  type: string;
+  id: string;
+  name: string;
+  value: string;
+  handleChange: (e: any) => void;
 }
 
-const CustomInput =  ({placeholder, type, name, id}: Props): JSX.Element => (
-  <input
-    className="bg-white my-4 focus:outline-none bg-gray-200 focus:bg-white focus:shadow-xl rounded-full py-2 px-4 block w-full appearance-none leading-normal"
-    type={type}
-    name={name}
-    id={id}
-    placeholder={placeholder}
-  />
-);
+const CustomInput = ({
+  placeholder,
+  type,
+  name,
+  id,
+  value,
+  handleChange
+}: Props): JSX.Element => {
+  return (
+    <input
+      className="bg-white my-4 focus:outline-none bg-gray-200 focus:bg-white focus:shadow-xl rounded-full py-2 px-4 block w-full appearance-none leading-normal"
+      type={type}
+      name={name}
+      id={id}
+      placeholder={placeholder}
+      onChange={handleChange}
+      value={value}
+    />
+  );
+};
 
 CustomInput.propTypes = {
-    placeholder: PropTypes.string,
-    type: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-}
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func
+};
 
-export default CustomInput
+export default CustomInput;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,39 @@
 import * as React from "react";
 import Layout from "../components/layouts";
 import { NextPage } from "next";
-import CustomInput from '../components/customInput';
-import SocialIcons from '../components/SocialIcons';
+import CustomInput from "../components/customInput";
+import SocialIcons from "../components/SocialIcons";
+
+const encode = (data: any): string => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
 
 const AboutPage: NextPage = () => {
+  const [values, setValues] = React.useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = (e: any): void => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: any): void => {
+    e.preventDefault();
+    console.log(values);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...values })
+    })
+      .then(() => console.log("sent successfully"))
+      .catch(e => console.log(e));
+  };
+
   return (
     <Layout title="Divine Hycenth | Contact Me">
       <div className="container rounded-lg mx-auto h-full flex justify-between">
@@ -12,19 +41,31 @@ const AboutPage: NextPage = () => {
           <img src="/images/contact.svg" className="w-full " alt="Contact me" />
         </div>
         <div className="container p-6 md:px-16 mx-auto w-full shadow-lg custom-bg2 rounded-lg md:py-16">
-          <h2 className="text-4xl font-sans font-bold text-white">Contact me</h2>
+          <h2 className="text-4xl font-sans font-bold text-white">
+            Contact me
+          </h2>
 
           <form
-            method="POST"
-            data-netlify="true"
-            netlify-honeypot="bot-field"
+            // method="POST"
+            // data-netlify="true"
+            // netlify-honeypot="bot-field"
             name="Contact from divinehycenth.com"
+            onSubmit={handleSubmit}
           >
-           <div>
-              <CustomInput id="name" name="name" type="text" placeholder="Name" />
-           </div>
             <div>
               <CustomInput
+                value={values.name}
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Name"
+                handleChange={handleChange}
+              />
+            </div>
+            <div>
+              <CustomInput
+                handleChange={handleChange}
+                value={values.email}
                 name="email"
                 id="email"
                 type="email"
@@ -38,19 +79,23 @@ const AboutPage: NextPage = () => {
                 rows={5}
                 id="message"
                 name="message"
+                value={values.message}
+                onChange={handleChange}
               />
             </div>
             <br />
-            <div><button
-              type="submit"
-              className="w-full border-white text-white border-2 hover:text-black  hover:bg-white my-4 rounded-full px-7 font-sans hover:shadow-lg"
-            >
-              Send Message ➻
-            </button></div>
+            <div>
+              <button
+                type="submit"
+                className="w-full border-white text-white border-2 hover:text-black  hover:bg-white my-4 rounded-full px-7 font-sans hover:shadow-lg"
+              >
+                Send Message ➻
+              </button>
+            </div>
           </form>
-            <br />
-            <br />
-            <SocialIcons />
+          <br />
+          <br />
+          <SocialIcons />
         </div>
       </div>
     </Layout>
