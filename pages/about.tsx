c7cc127bda081689fb9cab72339a5e2c73a83f8c
DNature <divinehycenth@outlook.com>
2020-03-13 16:34:50 +0100

    deploy

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,7 +7,11 @@ import AboutMeMDX from '../posts/about-me.md';
 const AboutPage: NextPage = () =>{
   
   return (
-    <Layout title="Divine Hycenth | About Me">
+    <Layout
+      title="Divine Hycenth | About Me"
+      imageUrl="/images/avatar.png"
+      description="I'm a full stack software developer creating open source projects and writing about modern JavaScript, Typescript, Graphql, Python, Node.js, and development."
+    >
       <div className="lg:mx-10 md:mx-8">
         <AboutMeMDX />
       </div>
