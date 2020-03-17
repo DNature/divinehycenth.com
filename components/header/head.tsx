bb9408a1b0ee227334cea633718c9a1660c0275c
DNature <divinehycenth@outlook.com>
2020-03-17 23:09:26 +0100

    changed twitter card summary

--- components/header/head.tsx
+++ components/header/head.tsx
@@ -13,17 +13,64 @@ interface Props {
 const NextHead: NextPage<Props> = ({
   pageTitle = "Divine Hycenth 👋 Full Stack Software Developer",
   description = "I'm Divine Hycenth a full stack software developer creating open source projects and writing about modern JavaScript, Typescript, Graphql, Python, Node.js, and development.",
-  imageUrl="/images/avatar-small.png",
-  path=""
+  imageUrl = "/images/avatar-small.png",
+  path = ""
 }) => (
   <Head>
-      {pageTitle ? (
-        <title>{pageTitle}</title>
-      ) : (
-          <title>Divine Hycenth | Full Stack Software Developer Blog website</title>
-        )}
-    
-    <meta charSet="utf-8" />
+    {pageTitle ? (
+      <title>{pageTitle}</title>
+    ) : (
+      <title>Divine Hycenth | Full Stack Software Developer Blog website</title>
+    )}
+
+    <title>Divine Hycenth 👋 Full Stack Software Developer</title>
+    <meta name="title" content={pageTitle} />
+    <meta name="description" content={description} />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://divinehycenth.com/" />
+    <meta property="og:title" content={pageTitle} />
+    <meta property="og:description" content={description} />
+    <meta
+      property="og:image"
+      content={`https://divinehycenth.com${imageUrl}`}
+    />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://divinehycenth.com/" />
+    <meta property="twitter:title" content={pageTitle} />
+    <meta property="twitter:description" content={description} />
+    <meta
+      property="twitter:image"
+      content={`https://divinehycenth.com${imageUrl}`}
+    />
+
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Satisfy&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+);
+
+NextHead.propTypes = {
+  pageTitle: PropTypes.string,
+  description: PropTypes.string,
+  imageUrl: PropTypes.string,
+  path: PropTypes.string
+};
+
+export default NextHead;
+
+/*
+<meta charSet="utf-8" />
     <link rel="shortcut icon" href="/icons/favicon.ico" />
     <link rel="icon" type="image/ico" href="/icons/favicon.ico" />
     <link
@@ -49,73 +96,44 @@ const NextHead: NextPage<Props> = ({
 
     <link rel="canonical" href="https://divinehycenth.com" />
     <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-    {/* facebook */}
-   
-    
-      <meta property="og:image" content={`https://divinehycenth.com${imageUrl}`} />
-    <meta
-      property="og:url"
-      content={
-        `https://divinehycenth.com${path}`
-      }
-    />
 
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    {/* <meta name="theme-color" content="#15202b" /> */}
-      <meta name="description" content={description} />
-      <meta name="title" content={pageTitle} />
-    <meta itemProp="name" content="Divine Hycenth" />
-    <meta itemProp="description" content={description} />
-    <meta property="twitter:title" content={pageTitle} />
-    <meta name="twitter:description" content={description} />
-    <meta property="twitter:url" content={`https://divinehycenth.com${path}`} />
-      <meta name="twitter:site" content="https://twitter.com/DivineHycenth" />
-      <meta name="twitter:creator" content="@DivineHycenth" />
-      <meta property="twitter:card" content="summary_large_image" />
-   
-      <meta property="twitter:image" content={`https://divinehycenth.com${imageUrl}`} />
 
-    {/* <meta property="og:determiner" content="software" /> */}
-      <meta property="og:image" content={`https://divinehycenth.com${imageUrl}`}/>
-    <meta property="og:locale" content="en_US" />
-      <meta property="og:image:width" content="1000" />
-      <meta property="og:image:height" content="532" />
-    <meta property="og:site_name" content="Divine Hycenth" />
-    <meta property="og:type" content="website" />
+<meta property="og:image" content={`https://divinehycenth.com${imageUrl}`} />
+  <meta
+    property="og:url"
+    content={
+      `https://divinehycenth.com${path}`
+    }
+  />
 
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={pageTitle} />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+<meta name="description" content={description} />
+  <meta name="title" content={pageTitle} />
+  <meta itemProp="name" content="Divine Hycenth" />
+  <meta itemProp="description" content={description} />
+  <meta property="twitter:title" content={pageTitle} />
+  <meta name="twitter:description" content={description} />
+  <meta property="twitter:url" content={`https://divinehycenth.com${path}`} />
+  <meta name="twitter:site" content="https://twitter.com/DivineHycenth" />
+  <meta name="twitter:creator" content="@DivineHycenth" />
+  <meta property="twitter:card" content="summary_large_image" />
+
+  <meta property="twitter:image" content={`https://divinehycenth.com${imageUrl}`} />
+
+<meta property="og:image" content={`https://divinehycenth.com${imageUrl}`} />
+  <meta property="og:locale" content="en_US" />
+  <meta property="og:image:width" content="1000" />
+  <meta property="og:image:height" content="532" />
+  <meta property="og:site_name" content="Divine Hycenth" />
+  <meta property="og:type" content="website" />
+
+  <meta property="og:description" content={description} />
+  <meta property="og:title" content={pageTitle} />
 
    
-      {/* <meta property="og:image:secure_url" content={imageUrl} /> */}
-      <meta property="og:image:secure_url" content={`https://www.divinehycenth.com${imageUrl}`} />
-      <meta property="og:image:alt" content={description} />
-   
-
-    <link
-      href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Satisfy&display=swap"
-      rel="stylesheet"
-    />
-
-  </Head>
-);
-
-NextHead.propTypes = {
-  pageTitle: PropTypes.string,
-  description: PropTypes.string,
-  imageUrl: PropTypes.string,
-  path: PropTypes.string,
-};
-
-export default NextHead;
+<meta property="og:image:secure_url" content={`https://www.divinehycenth.com${imageUrl}`} />
+  <meta property="og:image:alt" content={description} />
+*/
